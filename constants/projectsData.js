266f85b8b2471dd721f7887448680196c1ad9e9b
Nathan Sanchez <NathanSanchezDev@outlook.com>
2024-01-20 18:24:04 -0500

    update project 1

--- constants/projectsData.js
+++ constants/projectsData.js
@@ -1,32 +1,32 @@
 export const projectsData = [
     {
         _id: '1',
-        title: "Project Title 1",
-        description: "Brief description of Project 1",
-        category: "React",
-        images: ["url-to-image-1.jpg"], // Array of image URLs
-        liveUrl: "url-to-live-project-1", // URL to live project
-        repoUrl: "url-to-github-repo-1", // URL to GitHub repository
-        stack: ["React", "Node.js", "CSS"], // Technologies used
-    },
-    {
-        _id: '2',
-        title: "Project Title 2",
-        description: "Brief description of Project 2",
-        category: "Next",
-        images: ["url-to-image-2.jpg"], // Array of image URLs
-        liveUrl: "url-to-live-project-2", // URL to live project
-        repoUrl: "url-to-github-repo-2", // URL to GitHub repository
-        stack: ["Angular", "Firebase", "HTML"], // Technologies used
-    },
-    {
-        _id: '3',
-        title: "Project Title 3",
-        description: "Brief description of Project 3",
+        title: "AfterSouls Sneaker Store",
+        description: "Single page application for a sneaker store",
         category: "Javascript",
-        images: ["url-to-image-2.jpg"], // Array of image URLs
-        liveUrl: "https://www.google.com", // URL to live project
-        repoUrl: "url-to-github-repo-2", // URL to GitHub repository
-        stack: ["Angular", "Firebase", "HTML"], // Technologies used
+        images: ["https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2hyZWV5YWEzOXg2dng4bHVhZXlyMm8xNjZ3OGNqNXoxYXN3czZ3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUDJRPEgoQBJ9jrVaX/giphy.gif",], // Array of image URLs
+        liveUrl: "https://sneaker-proj.vercel.app/", // URL to live project
+        repoUrl: "https://github.com/NathanSanchezDev/sneakerProj", // URL to GitHub repository
+        stack: ["Javascript", "CSS"], // Technologies used 
     },
+        //     {
+//         _id: '2',
+//         title: "Project Title 2",
+//         description: "Brief description of Project 2",
+//         category: "Next",
+//         images: ["url-to-image-2.jpg"], // Array of image URLs
+//         liveUrl: "url-to-live-project-2", // URL to live project
+//         repoUrl: "url-to-github-repo-2", // URL to GitHub repository
+//         stack: ["Angular", "Firebase", "HTML"], // Technologies used
+//     },
+//     {
+//         _id: '3',
+//         title: "Project Title 3",
+//         description: "Brief description of Project 3",
+//         category: "Javascript",
+//         images: ["url-to-image-2.jpg"], // Array of image URLs
+//         liveUrl: "https://www.google.com", // URL to live project
+//         repoUrl: "url-to-github-repo-2", // URL to GitHub repository
+//         stack: ["Angular", "Firebase", "HTML"], // Technologies used
+//     },
 ];
