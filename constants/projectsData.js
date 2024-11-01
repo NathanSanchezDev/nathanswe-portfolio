7815d5a10e98b5500451659ca0cbcc07b6ffd1d5
Nathan Sanchez <NathanSanchezDev@outlook.com>
2024-10-31 21:11:18 -0400

    Add project data

--- constants/projectsData.js
+++ constants/projectsData.js
@@ -1,15 +1,14 @@
 export const projectsData = [
     {
         _id: '1',
-        title: "AfterSouls Sneaker Store",
-        description: "Single page application for a sneaker store",
-        category: "Javascript",
-        images: ["https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2hyZWV5YWEzOXg2dng4bHVhZXlyMm8xNjZ3OGNqNXoxYXN3czZ3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUDJRPEgoQBJ9jrVaX/giphy.gif",], // Array of image URLs
-        liveUrl: "https://sneaker-proj.vercel.app/", // URL to live project
-        repoUrl: "https://github.com/NathanSanchezDev/sneakerProj", // URL to GitHub repository
-        stack: ["Javascript", "CSS"], // Technologies used 
+        title: "Adaptive AI",
+        description: "AI Development Image Stack Landing Page",
+        category: "Next",
+        images: ["projects/adaptive1.png", "projects/adaptive2.png", "projects/adaptive3.png", "projects/adaptive4.png"], // Array of image URLs
+        liveUrl: "https://adaptive.ai-as-a-service.info/", // URL to live project
+        stack: ["React", "NextJS", "Javascript"], // Technologies used
     },
-            {
+    {
         _id: '2',
         title: "TrakFunding",
         description: "Personal Finance Tracking App",
@@ -19,14 +18,33 @@ export const projectsData = [
         repoUrl: "https://github.com/NathanSanchezDev/TrakFunding", // URL to GitHub repository
         stack: ["React", "NextJS", "Typescript"], // Technologies used
     },
-//     {
-//         _id: '3',
-//         title: "Project Title 3",
-//         description: "Brief description of Project 3",
-//         category: "Javascript",
-//         images: ["url-to-image-2.jpg"], // Array of image URLs
-//         liveUrl: "https://www.google.com", // URL to live project
-//         repoUrl: "url-to-github-repo-2", // URL to GitHub repository
-//         stack: ["Angular", "Firebase", "HTML"], // Technologies used
-//     },
+    {
+        _id: '3',
+        title: "PRism.dev",
+        description: "AI Powered Code Review (Web Based)",
+        category: "Next",
+        images: ["projects/prism1.png", "projects/prism2.png", "projects/prism3.png"], // Array of image URLs
+        liveUrl: "https://prism-ai-dev.vercel.app", // URL to live project
+        stack: ["React", "NextJS", "Typescript"], // Technologies used
+    },
+    {
+        _id: '4',
+        title: "AfterSouls Sneaker Store",
+        description: "Single page application for a sneaker store",
+        category: "Javascript",
+        images: ["https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2hyZWV5YWEzOXg2dng4bHVhZXlyMm8xNjZ3OGNqNXoxYXN3czZ3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUDJRPEgoQBJ9jrVaX/giphy.gif",], // Array of image URLs
+        liveUrl: "https://sneaker-proj.vercel.app/", // URL to live project
+        repoUrl: "https://github.com/NathanSanchezDev/sneakerProj", // URL to GitHub repository
+        stack: ["Javascript", "CSS"], // Technologies used 
+    },
+    //     {
+    //         _id: '3',
+    //         title: "Project Title 3",
+    //         description: "Brief description of Project 3",
+    //         category: "Javascript",
+    //         images: ["url-to-image-2.jpg"], // Array of image URLs
+    //         liveUrl: "https://www.google.com", // URL to live project
+    //         repoUrl: "url-to-github-repo-2", // URL to GitHub repository
+    //         stack: ["Angular", "Firebase", "HTML"], // Technologies used
+    //     },
 ];
