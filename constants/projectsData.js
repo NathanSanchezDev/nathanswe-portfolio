a6480cd64afecca57412091230b2a759ce6205c8
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-01-20 23:46:12 -0500

    New project data

--- constants/projectsData.js
+++ constants/projectsData.js
@@ -1,50 +1,66 @@
 export const projectsData = [
     {
+        _id: '5',
+        title: "AI Helper Library",
+        description: "A C#/.NET library for simplifying interactions with OpenAI's API",
+        category: "C# .NET",
+        images: [
+            "projects/ailibhelp.png",
+        ],
+        liveUrl: "https://www.nuget.org/packages/AIHelperLibrary/",
+        repoUrl: "https://github.com/NathanSanchezDev/ai-helper-library",
+        stack: ["C#", ".NET", "OpenAI"]
+    },
+    {
         _id: '1',
         title: "Adaptive AI",
         description: "AI Development Image Stack Landing Page",
         category: "Next",
-        images: ["projects/adaptive1.png", "projects/adaptive2.png", "projects/adaptive3.png", "projects/adaptive4.png"], // Array of image URLs
-        liveUrl: "https://adaptive.ai-as-a-service.info/", // URL to live project
-        stack: ["React", "NextJS", "Javascript"], // Technologies used
+        images: [
+            "projects/adaptive1.png",
+            "projects/adaptive2.png",
+            "projects/adaptive3.png",
+            "projects/adaptive4.png"
+        ],
+        liveUrl: "https://adaptive.ai-as-a-service.info/",
+        stack: ["React", "NextJS", "Javascript"]
     },
     {
         _id: '2',
         title: "TrakFunding",
         description: "Personal Finance Tracking App",
         category: "Next",
-        images: ["projects/trakfundingdash.jpg", "projects/trakfundingreg.jpg"], // Array of image URLs
-        liveUrl: "https://trakfunding.vercel.app/", // URL to live project
-        repoUrl: "https://github.com/NathanSanchezDev/TrakFunding", // URL to GitHub repository
-        stack: ["React", "NextJS", "Typescript"], // Technologies used
+        images: [
+            "projects/trakfundingdash.jpg",
+            "projects/trakfundingreg.jpg"
+        ],
+        liveUrl: "https://trakfunding.vercel.app/",
+        repoUrl: "https://github.com/NathanSanchezDev/TrakFunding",
+        stack: ["React", "NextJS", "Typescript"]
     },
     {
         _id: '3',
         title: "PRism.dev",
         description: "AI Powered Code Review (Web Based)",
         category: "Next",
-        images: ["projects/prism1.png", "projects/prism2.png", "projects/prism3.png"], // Array of image URLs
-        liveUrl: "https://prism-ai-dev.vercel.app", // URL to live project
-        stack: ["React", "NextJS", "Typescript"], // Technologies used
+        images: [
+            "projects/prism1.png",
+            "projects/prism2.png",
+            "projects/prism3.png"
+        ],
+        liveUrl: "https://prism-ai-dev.vercel.app",
+        stack: ["React", "NextJS", "Typescript"]
     },
     {
         _id: '4',
         title: "AfterSouls Sneaker Store",
         description: "Single page application for a sneaker store",
         category: "Javascript",
-        images: ["https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2hyZWV5YWEzOXg2dng4bHVhZXlyMm8xNjZ3OGNqNXoxYXN3czZ3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUDJRPEgoQBJ9jrVaX/giphy.gif",], // Array of image URLs
-        liveUrl: "https://sneaker-proj.vercel.app/", // URL to live project
-        repoUrl: "https://github.com/NathanSanchezDev/sneakerProj", // URL to GitHub repository
-        stack: ["Javascript", "CSS"], // Technologies used 
-    },
-    //     {
-    //         _id: '3',
-    //         title: "Project Title 3",
-    //         description: "Brief description of Project 3",
-    //         category: "Javascript",
-    //         images: ["url-to-image-2.jpg"], // Array of image URLs
-    //         liveUrl: "https://www.google.com", // URL to live project
-    //         repoUrl: "url-to-github-repo-2", // URL to GitHub repository
-    //         stack: ["Angular", "Firebase", "HTML"], // Technologies used
-    //     },
-];
+        images: [
+            "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExc2hyZWV5YWEzOXg2dng4bHVhZXlyMm8xNjZ3OGNqNXoxYXN3czZ3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xUDJRPEgoQBJ9jrVaX/giphy.gif"
+        ],
+        liveUrl: "https://sneaker-proj.vercel.app/",
+        repoUrl: "https://github.com/NathanSanchezDev/sneakerProj",
+        stack: ["Javascript", "CSS"]
+    }
+];  
