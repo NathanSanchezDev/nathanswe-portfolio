971e4c5f7f2f21f8d41c855a49514cae2c15b34e
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-01-21 00:10:53 -0500

    add analytics vercel

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Loading from "./loading";
 import "styles/globals.css";
 import { ThemeContext } from "context";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = { ...AppMetadata };
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
 					<Suspense fallback={<Loading />}>{children}</Suspense>
 					<AppFooter />
 				</ThemeContext>
+				<Analytics />
 				<SpeedInsights />
 			</body>
 		</html>
