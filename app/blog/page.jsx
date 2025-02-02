ad2eccadc55ca4dea33d2bdb9a8a7857362669a7
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-02-02 01:50:35 -0500

    revamp sorting of blog components / pages.

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -6,6 +6,8 @@ export const revalidate = 60;
 export default async function BlogPage() {
   const blogPosts = await getBlogPosts();
 
+  blogPosts.sort((a, b) => b.id - a.id);
+
   return (
     <section className="section">
       <div className="container-md">
--- app/page.js
+++ app/page.js
@@ -1,18 +1,28 @@
-import { WelcomeSection, AboutSection, TechnologiesSection, ProjectsSection } from "app/sections";
-import { LatestBlogs } from "app/sections/blog";
-import { getBlogPosts } from "lib/getBlogPosts";
-
-export default async function Page() {
+import {
+	WelcomeSection,
+	AboutSection,
+	TechnologiesSection,
+	ProjectsSection,
+  } from "app/sections";
+  import { LatestBlogs } from "app/sections/blog";
+  import { getBlogPosts } from "lib/getBlogPosts";
+  
+  export default async function Page() {
 	const blogPosts = await getBlogPosts();
-	const latestBlogs = blogPosts?.slice(0, 3) || [];
-
+  
+	const sortedBlogPosts = blogPosts
+	  ? [...blogPosts].sort((a, b) => Number(b.id) - Number(a.id))
+	  : [];
+  
+	const latestBlogs = sortedBlogPosts.slice(0, 3);
+  
 	return (
-		<div className="container-md">
-			<WelcomeSection />
-			<AboutSection />
-			<ProjectsSection />
-			<LatestBlogs blogs={latestBlogs} /> {/* Pass blogs as props */}
-			<TechnologiesSection />
-		</div>
+	  <div className="container-md">
+		<WelcomeSection />
+		<AboutSection />
+		<ProjectsSection />
+		<LatestBlogs blogs={latestBlogs} />
+		<TechnologiesSection />
+	  </div>
 	);
-}
+  }  
